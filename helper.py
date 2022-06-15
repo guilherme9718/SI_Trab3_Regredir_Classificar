b8d299b9ba942cd0e64ba6d0b616c9985c80d598
Guilherme Aguilar <guilhermeaguilarguii@gmail.com>
2022-06-15 18:34:55 -0300

    feito varios testes

--- helper.py
+++ helper.py
@@ -8,14 +8,11 @@ Created on Thu Jun  2 17:46:40 2022
 import pandas as pd
 
 def load_normalized_data_regression():
-    data = pd.read_csv('sinaisvitais_hist.txt', names=['id', 'pSis', 'pdiast', 'qPa', 'pulso', 'resp', 'grav', 'classe'])
+    X_variables, y_variables = load_data()
 
-    X_variables = data.iloc[:, 3:6]
     X_variables['qPa'] = (X_variables['qPa'] + 10.0) / 20.0
     X_variables['pulso'] = (X_variables['pulso']) / 200.0
     X_variables['resp'] = (X_variables['resp']) / 22.0
-    
-    y_variables = data.iloc[:, 6]
     
     return X_variables, y_variables
 
@@ -30,3 +27,14 @@ def load_normalized_data_classification():
     y_variables = data.iloc[:, 7]
     
     return X_variables, y_variables
+
+def load_data():
+    data = pd.read_csv('sinaisvitais_hist.txt', names=['id', 'pSis', 'pdiast', 'qPa', 'pulso', 'resp', 'grav', 'classe'])
+
+    X_variables = data.iloc[:, 3:6]
+    y_variables = data.iloc[:, 6]
+    return X_variables, y_variables
+
+def export_to_json(data):
+    import json
+    json.dump(data)
